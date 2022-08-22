d1cacd2f7f42f97a7f622d50ee0c6900017480d4
NarmathaGunasekaran <narmathanandhini09@gmail.com>
2022-08-22 17:51:53 +0530

    COMMITED

--- src/main/java/com/nseit/Spring/Application.java
+++ src/main/java/com/nseit/Spring/Application.java
@@ -11,7 +11,9 @@ public class Application {
 //		BinaryaSearchImpl binaryaSearch = new BinaryaSearchImpl();
 		ApplicationContext applicationContext = SpringApplication.run(Application.class, args);
 		BinaryaSearchImpl binaryaSearch = applicationContext.getBean(BinaryaSearchImpl.class);
-
+        BinaryaSearchImpl binaryaSearch2 = applicationContext.getBean(BinaryaSearchImpl.class);
+		System.out.println(binaryaSearch);
+		System.out.println(binaryaSearch2);
 		int result = binaryaSearch.binarySearch(new int[]{3,7,9,12},8);
 		System.out.println(result);
 	}
